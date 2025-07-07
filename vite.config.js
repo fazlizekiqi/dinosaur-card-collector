22f8eaae3694da127c5951d62ca43f13062b07df
Fazli Zekiqi <fazli.zekiqi@seb.se>
2025-07-07 12:06:42 +0200

    included repo name  in the vigte config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+    base: '/dinosaur-card-collector/', // use your GitHub repo name here
     build: {
         assetsInlineLimit: 0, // disable inlining entirely
         outDir: 'dist',
