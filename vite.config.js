7a7b866c00af7441cfb74597ad2bdb8ced4d90d1
Fazli Zekiqi <fazli.zekiqi@seb.se>
2026-06-10 21:55:12 +0200

    fix: correct base path from pokemon-card-collector to dinosaur-card-collector

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    base: '/pokemon-card-collector/pokemon-collector/', // pokemon-collector branch subfolder
+    base: '/dinosaur-card-collector/pokemon-collector/', // pokemon-collector branch subfolder
     build: {
         assetsInlineLimit: 0, // disable inlining entirely
         outDir: 'dist',
